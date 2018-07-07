c2cf01fba949debaf72ebfe164c70d07e5b5297e
Tony Jiang <tonyjiang02@gmail.com>
2018-07-07 00:27:59 -0700

    email storing

---
+++ saveEmail.js
@@ -0,0 +1,34 @@
+var serverAddress = "https://siliconvalleyyouth.herokuapp.com/email";
+$(document).ready(function() {
+	$("#submit-email").click(function() {
+		console.log("button clicked");
+		var email = $("#email-input").val();
+		var data = {
+			email:email
+		}
+		console.log(email);
+		// $.ajax({
+		// 	type:"POST",
+		// 	url:"https://siliconvalleyyouth.herokuapp.com/email",
+		// 	contentType:"application/json",
+		// 	data:JSON.stringify(data),
+		// 	error:function(xhr,status,error) {
+		// 		console.log(xhr);
+		// 	}
+		// })
+		jQuery.support.cors=true;
+		$.ajax({
+			type:"POST",
+			url:"http://localhost:3000/email",
+			crossDomain:true
+			// contentType:"application/json",
+			// data:JSON.stringify(data),
+			// error:function(xhr,status,error) {
+			// 	console.log(xhr);
+			// }
+		})
+		// $.post(serverAddress, JSON.stringify(data), function(d, status,xhr) {
+		// 	console.log(xhr);
+		// })
+	})
+})
