27a870969e0ebe935627e6bc6a5937c6b116cd7f
Tony Jiang <tonyjiang02@gmail.com>
2018-07-09 10:56:10 -0700

    added alert

--- saveEmail.js
+++ saveEmail.js
@@ -1,8 +1,8 @@
 var serverAddress = "https://siliconvalleyyouth.herokuapp.com/email";
 function run() {
-	alert("Thank you for signing up for our email list");
 	var xhr = new XMLHttpRequest();
 	var email = $("#email-input").val();
+	$("#email-input").val("Thank you for signing up for our email list!");
 	xhr.open("POST", serverAddress,true);
 	xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
 	xhr.send("email="+email);
