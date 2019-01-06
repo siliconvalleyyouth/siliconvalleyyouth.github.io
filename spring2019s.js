04af5b56432c5a056156c973c0750545148a9475
Sam Yang <samuel.q.yang@gmail.com>
2019-01-06 13:57:24 -0800

    testing ready

---
+++ spring2019s.js
@@ -0,0 +1,33 @@
+function Class2018(selector,classname, datestr, locationstr, classNumber, spreadsheetURL) {
+    this.selector = selector;
+    this.classname      = classname;
+    this.datestr        = datestr;
+    this.locationstr    = locationstr;
+    this.classNumber    = classNumber;
+    this.spreadsheetURL = spreadsheetURL;
+}
+var classArray = [];
+classArray.push(new Class2018('introweb',"Intro to Web Design", "Sunday 5-6:30pm", "Omei Academy", 8, ""));
+classArray.push(new Class2018('introjava1',"Intro to Java", "Sunday 5-6:30pm", "Omei Academy", 8, ""));
+classArray.push(new Class2018('amc8',"AMC 8", "Sunday 5-6:30pm", "Omei Academy", 10, ""));
+classArray.push(new Class2018('ml',"Intro to Machine Learning", "Sunday 6:45-8:15pm", "Omei Academy", 9, ""));
+classArray.push(new Class2018('physics',"Physics", "Sunday 6:45-8:15pm", "Omei Academy", 8, ""));
+classArray.push(new Class2018('introjava2',"Intro to Java", "Saturday 5:15-6:45pm", "Omei Academy", 8, ""));
+classArray.push(new Class2018('usaco',"USACO Bronze", "Saturday 5:15-6:45pm", "Omei Academy", 8, ""));
+classArray.push(new Class2018('interjava',"Intermediate Java", "Saturday 5:15-6:45pm", "Omei Academy", 8, ""));
+classArray.push(new Class2018('psd1',"Public Speaking and Debate", "Saturday 7-8:30pm", "Omei Academy", 8, ""));
+classArray.push(new Class2018('bio',"Biology", "Saturday 7-8:30pm", "Omei Academy", 8, ""));
+classArray.push(new Class2018('mathcounts',"MathCounts", "Saturday 10:30am-12pm", "Saratoga Fire Station", 8, ""));
+classArray.push(new Class2018('sb',"Science Bowl", "Saturday 1-2:30pm", "Saratoga Fire Station", 6, ""));
+classArray.push(new Class2018('advancedweb',"Advanced Web Design", "Saturday 5:30-6:30pm", "Saratoga Fire Station", 6, ""));
+classArray.push(new Class2018('ps',"Public Speaking", "Saturday 6:45-8pm", "Saratoga Fire Station", 8, ""));
+classArray.push(new Class2018('csce',"Competitive Speech and Current Events", "Sunday 2:45-4:15pm", "Saratoga Fire Station", 8, ""));
+classArray.push(new Class2018('whistory',"World History", "Sunday 4:30-5:30", "Saratoga Fire Station", 8, ""));
+classArray.push(new Class2018('amc8advanced',"AMC 8 Advanced", "Saturday 4-5:30pm", "Gunn High School", 7, ""));
+classArray.push(new Class2018('intropython',"Intro to Python", "Saturday 4-5:30pm", "Gunn High School", 8, ""));
+classArray.push(new Class2018('grammar',"Grammar and Vocab", "Friday 7-8:30pm", "Miller Middle School", 8, ""));
+classArray.push(new Class2018('bridge',"Bridge", "Friday 7-8:30pm", "Miller Middle School", 8, ""));
+classArray.push(new Class2018('psd2',"Public Speaking and Debate", "Friday 7-8:30pm", "Miller Middle School", 6, ""));
+classArray.push(new Class2018('psce2',"Public Speaking and Current Events", "Friday 7-8:30pm", "Miller Middle School", 8, ""));
+classArray.push(new Class2018('basketball',"Basketball", "Friday 5-7pm", "Dilworth Elementary School", 8, ""));
+classArray.push(new Class2018('tennis',"Tennis", "Sunday 3:30-5pm", "Saratoga Oaks Tennis Courts", 8, ""));
