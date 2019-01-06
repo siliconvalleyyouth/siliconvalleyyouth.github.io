3a33aedd83b6a6525827c100f6a8191c52f92686
bruisedllama <tonyjiang02@gmail.com>
2019-01-05 19:42:32 -0800

    added class selectors, prices, and more

--- payment.js
+++ payment.js
@@ -1,10 +1,11 @@
+var classArray = classArray;
 var stripe = Stripe('pk_test_txUHW0roiaw7rDEneBF5IgCB');
 var elements = stripe.elements();
 document.addEventListener("DOMContentLoaded", function (event) {
     createElements();
-    formHandler()
+    formHandler();
+    initSelector();
 });
-
 // Custom styling can be passed to options when creating an Element.
 var style = {
     base: {
@@ -13,6 +14,54 @@ var style = {
         color: "#32325d",
     }
 };
+function calcPrice() {
+    console.log("get price");
+    var priceDisplay = document.getElementById("costDisplay");
+    var select = document.getElementById("ClassSelect");
+    var index = select.selectedIndex;
+    var noDisabled = [];
+    console.log(select.childNodes);
+    for(var i =0; i<select.childNodes.length; i++) {
+        if(typeof select.childNodes[i].attributes != 'undefined') {
+            noDisabled.push(select.childNodes[i]);
+        }else {
+            console.log("disabled option");
+        }
+    }
+    var sessions = noDisabled[index].attributes[0].nodeValue;
+    console.log(sessions);
+    priceDisplay.innerHTML = "Cost "+ parseInt(sessions,10) * 15 + "$";
+    document.getElementById("priceVar").value = parseInt(sessions,10) * 15;
+}
+function initSelector() {
+    console.log("initializing Selector")
+    var selector = document.getElementById("ClassSelect");
+    var location = classArray[0].locationstr;
+    //Location marker
+    var blank = document.createElement('option');
+    selector.appendChild(blank);
+    var disabledLocation = document.createElement('option');
+    disabledLocation.setAttribute('disabled', "");
+    disabledLocation.innerHTML = location;
+    selector.appendChild(disabledLocation);
+    for (var i = 0; i < classArray.length; i++) {
+        if (classArray[i].locationstr == location) {
+
+        } else {
+            location = classArray[i].locationstr;
+            var disabledLocation = document.createElement('option');
+            disabledLocation.setAttribute('disabled', "");
+            disabledLocation.innerHTML=location;
+            selector.appendChild(disabledLocation);
+        }
+        var option = document.createElement('option');
+        var inner = classArray[i].classname + ", " + classArray[i].locationstr + ", " + classArray[i].datestr + ", " + classArray[i].classNumber+ " sessions";
+        option.setAttribute("data-class-number", classArray[i].classNumber);
+        console.log("set classno")
+        option.innerHTML= inner;
+        selector.appendChild(option);
+    }
+}
 var card = elements.create('card', { style: style });
 card.addEventListener('change', function (event) {
     var displayError = document.getElementById('card-errors');
---
+++ spring2018.js
@@ -0,0 +1,32 @@
+function Class2018(classname, datestr, locationstr, classNumber, spreadsheetURL) {
+    this.classname      = classname;
+    this.datestr        = datestr;
+    this.locationstr    = locationstr;
+    this.classNumber    = classNumber;
+    this.spreadsheetURL = spreadsheetURL;
+}
+var classArray = [];
+classArray.push(new Class2018("Intro to Web Design", "Sunday 5-6:30pm", "Omei Academy", 8, ""));
+classArray.push(new Class2018("Intro to Java", "Sunday 5-6:30pm", "Omei Academy", 8, ""));
+classArray.push(new Class2018("AMC 8", "Sunday 5-6:30pm", "Omei Academy", 10, ""));
+classArray.push(new Class2018("Intro to Machine Learning", "Sunday 6:45-8:15pm", "Omei Academy", 9, ""));
+classArray.push(new Class2018("Physics", "Sunday 6:45-8:15pm", "Omei Academy", 8, ""));
+classArray.push(new Class2018("Intro to Java", "Saturday 5:15-6:45pm", "Omei Academy", 8, ""));
+classArray.push(new Class2018("USACO Bronze", "Saturday 5:15-6:45pm", "Omei Academy", 8, ""));
+classArray.push(new Class2018("Intermediate Java", "Saturday 5:15-6:45pm", "Omei Academy", 8, ""));
+classArray.push(new Class2018("Public Speaking and Debate", "Saturday 7-8:30pm", "Omei Academy", 8, ""));
+classArray.push(new Class2018("Biology", "Saturday 7-8:30pm", "Omei Academy", 8, ""));
+classArray.push(new Class2018("MathCounts", "Saturday 10:30am-12pm", "Saratoga Fire Station", 8, ""));
+classArray.push(new Class2018("Science Bowl", "Saturday 1-2:30pm", "Saratoga Fire Station", 6, ""));
+classArray.push(new Class2018("Advanced Web Design", "Saturday 5:30-6:30pm", "Saratoga Fire Station", 6, ""));
+classArray.push(new Class2018("Public Speaking", "Saturday 6:45-8pm", "Saratoga Fire Station", 8, ""));
+classArray.push(new Class2018("Competitive Speech and Current Events", "Sunday 2:45-4:15pm", "Saratoga Fire Station", 8, ""));
+classArray.push(new Class2018("World History", "Sunday 4:30-5:30", "Saratoga Fire Station", 8, ""));
+classArray.push(new Class2018("AMC 8 Advanced", "Saturday 4-5:30pm", "Gunn High School", 7, ""));
+classArray.push(new Class2018("Intro to Python", "Saturday 4-5:30pm", "Gunn High School", 8, ""));
+classArray.push(new Class2018("Grammar and Vocab", "Friday 7-8:30pm", "Miller Middle School", 8, ""));
+classArray.push(new Class2018("Bridge", "Friday 7-8:30pm", "Miller Middle School", 8, ""));
+classArray.push(new Class2018("Public Speaking and Debate", "Friday 7-8:30pm", "Miller Middle School", 6, ""));
+classArray.push(new Class2018("Public Speaking and Current Events", "Friday 7-8:30pm", "Miller Middle School", 8, ""));
+classArray.push(new Class2018("Basketball", "Friday 5-7pm", "", 8, ""));
+classArray.push(new Class2018("Tennis", "Sunday 3:30-5pm", "Saratoga Oaks Tennis Courts", 8, ""));
