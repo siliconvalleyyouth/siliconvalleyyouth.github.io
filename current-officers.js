3aaa0c5189d25c0f3ff4f9105b9f1fb32abb93dd
KevinS <36679585+KevinS78@users.noreply.github.com>
2022-02-26 14:39:41 -0800

    Add Eric Wang information as VP of Math
    Add Eric Wang information as VP of Math

--- current-officers.js
+++ current-officers.js
@@ -26,7 +26,7 @@ var Officers20212022 = [
     ["Shreyas Rana", "VP of CS", "As the Vice President of Computer Science, Shreyas Rana oversees the CS department classes and assists new teachers. He has been teaching Intermediate Java for 3 semesters so far. Shreyas enjoys being a part of Silicon Valley Youth because he is able to share his experiences in Computer Science with other students in his community."],
     ["Max Zhang", "VP of Humanities", "As the Vice President of Humanities, Max Zhang handles the course management of the Humanities department. He has taught US History for 8 semesters now, as well as Basketball and Grammar/Vocab in the past, for a total of 5 years now. Max enjoys passing on his knowledge to younger students and watching them learn and grow throughout the course in each semester."],
     ["Andy Xu", "VP of Public Speaking and Debate", ""],
-    ["Eric Wang", "VP of Math", ""],
+    ["Eric Wang", "VP of Math", "Eric is a sophomore at Lynbrook High School, and this is his fourth semester teaching at SVY having taught both AMC 8 Basic and Advanced. He has been doing competition math since the start of 8th grade and has achieved numerous awards including being a 3-time AIME qualifier, an AMC 10 DHR member, and hopefully a USAJMO qualifier this year. Along with math, Eric also enjoys coding and playing soccer and is a member of his school junior varsity soccer team."],
     ["Kaitlyn Wang", "Assisstant VP of Math", ""],
     ["Michael Song", "Facilities Director", ""],
     ["Charles Li", "Facilities Director", ""],
