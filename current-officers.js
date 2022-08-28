dfb5581c24da7f97a4b3d696343c90a4e0299f8e
KevinS <hanwenshi6@gmail.com>
2022-08-27 17:18:47 -0700

    updates pt1

--- current-officers.js
+++ current-officers.js
@@ -11,35 +11,33 @@ function makeOpenBioCallback(name, position, description) {
         openBioAction(name, position, description);
     }
 }
-
-var Officers20212022 = [
-    ["Justin Gu", "EVP of Operations", ""],
-    ["Michael Ma", "EVP of Marketing", "As the Executive Vice President of Marketing, Michael Ma oversees advertising for classes at the beginning of semesters as well as handles general public relations. He has been teaching various Public Speaking classes at SVY for nine sessions. Michael enjoys being able to share his public speaking passion with younger students as well as having the opportunity to teach and work in a classroom environment."],
-    ["Annli Zhu", "EVP of Human Resources", ""],
-    ["Anping Zhu", "VP of Learning and Development", "As the VP of Learning and Development, Anping Zhu carries out teacher training sessions to improve teaching quality and assists in developing curriculum with new teachers. In the past, he has taught World History and Model United Nations, for a combined three semesters. SVY has been a valuable experience for him, improving his leadership and organizational skills."],
-    ["Cayden Gu", "VP of Technology", ""],
-    ["Tony Zhou", "Webmaster", "As one of the two webmasters, Tony Zhou works to maintain the website by uploading teacher bios, class descriptions, teacher headshots, and class signup lists. He has been teaching various classes ranging from public speaking to web design at SVY for 6 sessions. Tony enjoys teaching and having fun with his students at SVY, and loves seeing his younger students grow and develop as they progress through his classes."],
-    ["Kevin Shi", "Webmaster", ""],
-    ["Aiden Ye", "VP of Finance", "As the VP of finance, Aiden manages the funds of the organization, and handles reimbursement requests as well as refunds. Aiden has been teaching mathematics and sciencebowl over the past two years, and enjoys sharing his experience as well as helping students perform better. He enjoys teaching at because it is a great way for him to help motivate new students and hopes to help his community more with SVY."],
-    ["Tim Jing", "VP of Events", ""],
-    ["Samika Swamy", "VP of Science", ""],
-    ["Shreyas Rana", "VP of CS", "As the Vice President of Computer Science, Shreyas Rana oversees the CS department classes and assists new teachers. He has been teaching Intermediate Java for 3 semesters so far. Shreyas enjoys being a part of Silicon Valley Youth because he is able to share his experiences in Computer Science with other students in his community."],
-    ["Max Zhang", "VP of Humanities", "As the Vice President of Humanities, Max Zhang handles the course management of the Humanities department. He has taught US History for 8 semesters now, as well as Basketball and Grammar/Vocab in the past, for a total of 5 years now. Max enjoys passing on his knowledge to younger students and watching them learn and grow throughout the course in each semester."],
-    ["Andy Xu", "VP of Public Speaking and Debate", ""],
-<<<<<<< Updated upstream
-    ["Eric Wang", "VP of Math", "Eric is a sophomore at Lynbrook High School, and this is his fourth semester teaching at SVY having taught both AMC 8 Basic and Advanced. He has been doing competition math since the start of 8th grade and has achieved numerous awards including being a 3-time AIME qualifier, an AMC 10 DHR member, and hopefully a USAJMO qualifier this year. Along with math, Eric also enjoys coding and playing soccer and is a member of his school junior varsity soccer team."],
-=======
-    ["Eric Wang", "VP of Math", ""],
->>>>>>> Stashed changes
-    ["Michael Song", "Facilities Director", ""],
-    ["Charles Li", "Facilities Director", ""],
-    ["Rachel Chan", "Facilities Manager", "As a site director, Rachel Chan oversees the logistics of the organization’s facilities secures designated time slots for each class. Rachel has been Intermediate Java for the past three semesters and enjoys sharing her love for programming with others. Not only does she appreciate having the opportunity to expand the capabilities of youth through this organization, but she’s also very excited to see the impacts SVY will make in the upcoming future."],
+	
+	
+	
+	
+var Officers20222023 = [
+    ["Aiden Ye", "EVP of Marketing", ""],
+    ["Anping Zhu", "EVP of Academics", ""],
+    ["Cayden Gu","EVP of Operations",""],
+    ["Timothy Leung", "VP of Media", ""],
+    ["Minh Do", "VP of Media", ""],
+    ["Alison Yang", "VP of Teacher Development", ""],
+    ["Kevin Shi","VP of Technology",""],
+    ["Tony Zhou","VP of Technology",""],
+    ["Charles Li","VP of Finance & Site Director",""],
+    ["Jasmine Yuan","VP of Humanities",""],
+    ["Derek Li","VP of Humanities (Assistant)",""],
+    ["Grace Li","VP of Science",""],
+    ["Eric Wang","VP of Math",""],
+    ["Bryan Liu","VP of Debate & Speech",""],
+    ["Austin Liu","VP of CS",""],
+    ["Kyleen Liao","VP of CS (Assistant)",""],
 ];
 
 var container;
 var Officers;
-Officers = Officers20212022;
-container = document.getElementById("2021-2022")
+Officers = Officers20222023;
+container = document.getElementById("2022-2023")
 for (var i = 0; i < Officers.length; i++) {
     if (i % 3 == 0) {
         var row = document.createElement('div')
--- past-officers.js
+++ past-officers.js
@@ -1,3 +1,22 @@
+var Officers20212022 = [
+    ["Justin Gu", "EVP of Operations", ""],
+    ["Michael Ma", "EVP of Marketing", "As the Executive Vice President of Marketing, Michael Ma oversees advertising for classes at the beginning of semesters as well as handles general public relations. He has been teaching various Public Speaking classes at SVY for nine sessions. Michael enjoys being able to share his public speaking passion with younger students as well as having the opportunity to teach and work in a classroom environment."],
+    ["Annli Zhu", "EVP of Human Resources", ""],
+    ["Anping Zhu", "VP of Learning and Development", "As the VP of Learning and Development, Anping Zhu carries out teacher training sessions to improve teaching quality and assists in developing curriculum with new teachers. In the past, he has taught World History and Model United Nations, for a combined three semesters. SVY has been a valuable experience for him, improving his leadership and organizational skills."],
+    ["Cayden Gu", "VP of Technology", ""],
+    ["Tony Zhou", "Webmaster", "As one of the two webmasters, Tony Zhou works to maintain the website by uploading teacher bios, class descriptions, teacher headshots, and class signup lists. He has been teaching various classes ranging from public speaking to web design at SVY for 6 sessions. Tony enjoys teaching and having fun with his students at SVY, and loves seeing his younger students grow and develop as they progress through his classes."],
+    ["Kevin Shi", "Webmaster", ""],
+    ["Aiden Ye", "VP of Finance", "As the VP of finance, Aiden manages the funds of the organization, and handles reimbursement requests as well as refunds. Aiden has been teaching mathematics and sciencebowl over the past two years, and enjoys sharing his experience as well as helping students perform better. He enjoys teaching at because it is a great way for him to help motivate new students and hopes to help his community more with SVY."],
+    ["Tim Jing", "VP of Events", ""],
+    ["Samika Swamy", "VP of Science", ""],
+    ["Shreyas Rana", "VP of CS", "As the Vice President of Computer Science, Shreyas Rana oversees the CS department classes and assists new teachers. He has been teaching Intermediate Java for 3 semesters so far. Shreyas enjoys being a part of Silicon Valley Youth because he is able to share his experiences in Computer Science with other students in his community."],
+    ["Max Zhang", "VP of Humanities", "As the Vice President of Humanities, Max Zhang handles the course management of the Humanities department. He has taught US History for 8 semesters now, as well as Basketball and Grammar/Vocab in the past, for a total of 5 years now. Max enjoys passing on his knowledge to younger students and watching them learn and grow throughout the course in each semester."],
+    ["Andy Xu", "VP of Public Speaking and Debate", ""],
+    ["Eric Wang", "VP of Math", "Eric is a sophomore at Lynbrook High School, and this is his fourth semester teaching at SVY having taught both AMC 8 Basic and Advanced. He has been doing competition math since the start of 8th grade and has achieved numerous awards including being a 3-time AIME qualifier, an AMC 10 DHR member, and hopefully a USAJMO qualifier this year. Along with math, Eric also enjoys coding and playing soccer and is a member of his school junior varsity soccer team."],
+    ["Michael Song", "Facilities Director", ""],
+    ["Charles Li", "Facilities Director", ""],
+    ["Rachel Chan", "Facilities Manager", "As a site director, Rachel Chan oversees the logistics of the organization’s facilities secures designated time slots for each class. Rachel has been Intermediate Java for the past three semesters and enjoys sharing her love for programming with others. Not only does she appreciate having the opportunity to expand the capabilities of youth through this organization, but she’s also very excited to see the impacts SVY will make in the upcoming future."],
+];
 var Officers20202021 = [
     ["Oliver Ye", "President", "As the President of Silicon Valley Youth, Oliver Ye oversees internal and external operations of the organization. Over the past five years, Oliver has helped expand SVY from 3 to over 60 teachers, also collaborating with superintendents from the Ravenswood and Franklin McKinley districts to plan projects. At SVY, he has been teaching for 10 semesters now, focusing mainly on science bowl. Oliver's favorite part of SVY is seeing both students and teachers grow, learning lessons and stepping up to become leaders in their own community."],    
     ["Cynthia Jia", "EVP of Human Resources", "As the Executive Vice President of Human Resources, Cynthia Jia oversees the teacher interview process, establishes teacher training, and integrates class evaluation plans to ensure course quality. She has been teaching Biology for five sessions. Cynthia is passionate about working with young learners and inspiring students with scientific knowledge, creating a positive and fun environment for them to thrive."],
@@ -76,21 +95,24 @@ for (var m=0; m<6; m++) {
     var container;
     var Officers;
     if(m==0) {
+        Officers = Officers20212022;
+        container = document.getElementById("2021-2022")
+    }if(m==1) {
         Officers = Officers20202021;
         container = document.getElementById("2020-2021")
-    }else if(m==1) {
+    }else if(m==2) {
         Officers = Officers20192020;
         container = document.getElementById("2019-2020")
-    }else if (m==2){
+    }else if (m==3){
         Officers = Officers20182019;
         container = document.getElementById("2018-2019")
-    }else if (m==3) {
+    }else if (m==4) {
         Officers = Officers20172018;
         container = document.getElementById("2017-2018")
-    }else if (m==4) {
+    }else if (m==5) {
         Officers = Officers20162017;
         container = document.getElementById("2016-2017")
-    }else if (m==5) {
+    }else if (m==6) {
         Officers = Officers20152016;
         container = document.getElementById("2015-2016")
     }
