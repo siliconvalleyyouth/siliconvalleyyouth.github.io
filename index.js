49f4f81a38282529e88f02282e8db09e1bac8cdc
KevinS <36679585+KevinS78@users.noreply.github.com>
2022-12-28 13:04:59 -0800

    Update index.js

--- index.js
+++ index.js
@@ -216,7 +216,7 @@ $("#mainnav").html("<li class=\"navig\"><a href=\"https://www.siliconvalleyyouth
 
 //getting rid of contact link
 
-$("#footerlinks").html("<a href=\"team.html\">Our Team</a> <a href=\"donate.html\">Donate</a> <a href=\"mission.html\">Mission</a>");
+$("#footerlinks").html("<a href=\"/team.html\">Our Team</a> <a href=\"donate.html\">Donate</a> <a href=\"mission.html\">Mission</a>");
 
 
 
