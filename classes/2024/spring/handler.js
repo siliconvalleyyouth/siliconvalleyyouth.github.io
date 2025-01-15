94b449ca9e805fe3627afc4135c1c2081b3049a5
sky2025 <66568120+sky2025@users.noreply.github.com>
2025-01-14 20:49:28 -0800

    Update handler.js

--- classes/2024/spring/handler.js
+++ classes/2024/spring/handler.js
@@ -76,7 +76,7 @@ $(document).ready(function() {
         type: "GET",
         contentType: 'application/json',
         // url : "http://localhost:3000/class2024spring?id="+id,
-        url : "https://siliconvalleyyouth-current.herokuapp.com/class2024spring?id="+id,
+        url : "https://siliconvalleyyouth.herokuapp.com/class2024spring?id="+id,
         dataType: "json",
         success: function(res) {
             console.log("success")
